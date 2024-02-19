6eb6a41ae00b72c748280947ed1384c29d5866e1
sonnekea <soso0002@stud.kea.dk>
2024-02-19 09:56:22 +0100

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int StudentAge = 15;
+        double StudentGPA = 3.45;
+        char StudentFirstInitials = 'K';
+        char StudentLastInitials = 'H';
+
+
+    }
